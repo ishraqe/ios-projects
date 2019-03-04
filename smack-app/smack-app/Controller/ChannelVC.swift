afc8fb85b337d24309ac7fe234245140435b5bd2
ishraqe <kanak431@diu.edu.bd>
2019-03-04 18:21:08 +0600

    drawer reveal controller

---
+++ smack-app/smack-app/Controller/ChannelVC.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60        
+    }
+}
