31214954112a8adca21239c36d2910784b340deb
ishraqe <kanak431@diu.edu.bd>
2019-03-09 02:58:55 +0600

    url added

--- smack-app/smack-app/Utilities/Constants.swift
+++ smack-app/smack-app/Utilities/Constants.swift
@@ -1,8 +1,22 @@
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+
+//URL constants
+
+let BASE_URL = "https://smack-chat-ish.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 
 //seques
 
 let TO_Login = "toLogin"
 let To_Create_Account = "to_Create_account"
 let Unwind = "unwindToSeque"
+
+
+// User defaults
+
+let TOKEN_KEY = "key"
+let LOGGED_IN_KEY = "loggedin"
+let USER_EMAIL = "userEmail"
