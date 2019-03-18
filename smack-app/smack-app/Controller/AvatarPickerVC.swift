0ab18659e7fa6460444fba3bfcc8c30f008d76ba
ishraqe <kanak431@diu.edu.bd>
2019-03-18 18:08:52 +0600

    gn bg color

--- smack-app/smack-app/Controller/AvatarPickerVC.swift
+++ smack-app/smack-app/Controller/AvatarPickerVC.swift
@@ -30,8 +30,27 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 28
     }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var numberOfColums: CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numberOfColums = 4
+        }
+        
+        let spaceBetweenCells: CGFloat = 10
+        let padding: CGFloat = 40
+        let cellDimension = ((collectionView.bounds.width - padding) - (numberOfColums - 1) * spaceBetweenCells) / numberOfColums
+        
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
     
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        }else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func backPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
--- smack-app/smack-app/Controller/CreateAccountVC.swift
+++ smack-app/smack-app/Controller/CreateAccountVC.swift
@@ -10,12 +10,20 @@ class CreateAccountVC: UIViewController {
     // Default variables
     var avatarName = "profileDefault"
     var avatarColor = "[0.5,0.5,0.5,1]"
+    var bgColor:  UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
         view.addGestureRecognizer(tap)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
     }
     
     @IBAction func colseCreateVCPressed(_ sender: Any) {
@@ -25,6 +33,11 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
     }
     @IBAction func generateAvatarPressed(_ sender: Any) {
+        let r = CGFloat(arc4random_uniform(255)) / 255
+         let g = CGFloat(arc4random_uniform(255)) / 255
+         let b = CGFloat(arc4random_uniform(255)) / 255
+        bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        self.userImg.backgroundColor = bgColor
     }
     
     @IBAction func createAccount(_ sender: Any) {
