d269cf3f6a238370241d3f8345a7e8a69f850a23
ishraqe <kanak431@diu.edu.bd>
2019-04-02 17:19:39 +0600

    location added

--- pixel-city/pixel-city/controllers/MapVC.swift
+++ pixel-city/pixel-city/controllers/MapVC.swift
@@ -1,21 +1,43 @@
 
 import UIKit
-import Alamofire
-import AlamofireImage
+
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationService()
     }
+    
     @IBAction func centeeMapBtnWasPressed(_ sender: Any) {
     }
 }
 
 extension MapVC: MKMapViewDelegate {
-    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else {return}
+        let coordinateRegion = MKCoordinateRegion (center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationService() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
 
