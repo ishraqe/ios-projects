c5a060016d44fe2dd0086556fc8cdb4fe16be009
ishraqe <kanak431@diu.edu.bd>
2019-04-07 18:56:39 +0600

    text view not displayed

--- Goal Challenge/Goal Challenge/Controllers/CreateGoalVC.swift
+++ Goal Challenge/Goal Challenge/Controllers/CreateGoalVC.swift
@@ -9,16 +9,27 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType: GoalType = .shortTerm
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nextBtn.bindToKeyBoard()
         // Do any additional setup after loading the view.
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDelectedColor()
     }
     
     
     @IBAction func shortTrmBtnWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDelectedColor()
     }
     @IBAction func longTrmBtnWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermBtn.setSelectedColor()
+        shortTermBtn.setDelectedColor()
     }
     @IBAction func nextBtnWasPressed(_ sender: Any) {
     }
